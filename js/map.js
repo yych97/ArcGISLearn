7682dc364ced8a68f2541454947385ab638cf0ff
杨诣成 <yych97@126.com>
2018-08-21 15:50:35 +0800

    modify

--- js/map.js
+++ js/map.js
@@ -1,9 +1,26 @@
-require(["esri/Map", "esri/views/MapView", "dojo/domReady!"], function (Map, MapView) {
+$(function () {
+    //全选,设置chheckbox name='all' tbody id=tb
+    $("input[name=all]").click(function () {
+        if (this.checked) {
+            $("input[name=layers]").prop("checked", true);
+        } else {
+            $("input[name=layers]").prop("checked", false);
+        }
+    });
+});
+
+require(["esri/Map", "esri/views/MapView", "esri/layers/MapImageLayer", "dojo/domReady!"], function (Map, MapView, MapImageLayer) {
+    var tangLayer = new MapImageLayer({
+        url: "http://edugis.rchss.sinica.edu.tw/ArcGIS/rest/services/tsgis_tang/MapServer"
+    });
     var map = new Map({
         basemap: "streets"
-    })
+    });
+    map.add(tangLayer);
     var mapview = new MapView({
         map: map,
-        container: "map"
-    })
+        container: "map",
+        center: [110, 40],
+        zoom: 4
+    });
 });
