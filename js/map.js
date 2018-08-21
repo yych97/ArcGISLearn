16bf8abd0ab073005c3119303835fe214a64afcd
杨诣成 <yych97@126.com>
2018-08-21 20:52:12 +0800

    modify

--- js/map.js
+++ js/map.js
@@ -2,10 +2,18 @@ $(function () {
     //全选,设置chheckbox name='all' tbody id=tb
     $("input[name=all]").click(function () {
         if (this.checked) {
-            $("input[name=layers]").prop("checked", true);
+            $("input[name=time]").prop("checked", true);
         } else {
-            $("input[name=layers]").prop("checked", false);
+            $("input[name=time]").prop("checked", false);
         }
+    });
+    $("input[name=time-layers]").click(function(){
+        $("input[name=time-layers]").prop("checked", false); //设置当前选中的checkbox同级(兄弟级)其他checkbox状态为未选中
+        $(this).prop("checked", true);//设置当前选中checkbox的状态为checked
+    });
+    $("input[name=type-layers]").click(function(){
+        $("input[name=type-layers]").prop("checked", false); //设置当前选中的checkbox同级(兄弟级)其他checkbox状态为未选中
+        $(this).prop("checked", true);//设置当前选中checkbox的状态为checked
     });
 });
 
